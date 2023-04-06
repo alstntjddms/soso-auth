8ba7e548aecc7bb709e18c078d3ad5dde0f96d90
전민수 <79839332+alstntjddms@users.noreply.github.com>
2023-04-06 16:17:24 +0900

    Initial commit

---
+++ test1/index.js
@@ -0,0 +1,10 @@
+new Vue({
+    el: '#app',
+    data: {
+      people: [
+        { id: 1, name: 'John', age: 30 },
+        { id: 2, name: 'Jane', age: 25 },
+        { id: 3, name: 'Bob', age: 40 }
+      ]
+    }
+  });
