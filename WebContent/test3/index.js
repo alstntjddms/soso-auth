3428c61e730c94513e2af62525e7aa45e346d478
전민수 <79839332+alstntjddms@users.noreply.github.com>
2023-04-07 10:59:46 +0900

    전역 freamework함수 추가

--- WebContent/test3/index.js
+++ WebContent/test3/index.js
@@ -5,14 +5,6 @@ new Vue({
     ],
     letterCount: 0
   },
-  computed: {
-    formatTimestamp() {
-      return function(timestamp) {
-        const d = new Date(timestamp);
-        return d.toISOString().slice(0, 19).replace('T', ' ');
-      }
-    }
-  },
   mounted : async function(){
       await this.load();
       this.letterCount = this.letters.length
