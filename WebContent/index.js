cd7b087214444195c2ba902422e129dd975f1768
전민수 <79839332+alstntjddms@users.noreply.github.com>
2023-04-07 13:26:39 +0900

    authcode 확인 인증로직 추가

--- WebContent/index.js
+++ WebContent/index.js
@@ -1,5 +1,7 @@
+var accessYn = false;
 // 로그인 처리
 function login(){
+    if (accessYn == false) return;
     $("#sidebar").load("./sidebar/sidebar.html");
     $("#main").load("./test1/index.html");
 };
@@ -9,26 +11,33 @@ $("#main").load("./login/index.html");
 
 // 메뉴 클릭시 메인페이지 이벤트
 function test1(){
+    if (accessYn == false) return;
     $("#main").load("./test1/index.html");
 };
 function test2(){
+    if (accessYn == false) return;
     $("#main").load("./test2/index.html");
 };
 function test3(){
+    if (accessYn == false) return;
     $("#main").load("./test3/index.html");
 };
 function test4(){
+    if (accessYn == false) return;
     $("#main").load("./test4/index.html");
 };
 function test5(){
+    if (accessYn == false) return;
     $("#main").load("./test5/index.html");
 };
 function test6(){
+    if (accessYn == false) return;
     $("#main").load("./test6/index.html");
 };
 
 // 홈 클릭시 초기화
 function home(){
+    accessYn = false;
     $("#main").load("./login/index.html");
     $("#sidebar").html("");
 };
--- WebContent/login/index.js
+++ WebContent/login/index.js
@@ -5,21 +5,32 @@ var b = new Vue({
     errMsg: ""
   },
   mounted : async function(){
-      console.log("로그인페이지");
   },
   methods:{
     login : async function(){
-      console.log(this.authCode);
-      // this.members = await axios.get('https://plater.kr/api/memberall').then(function(response){
-      //   console.log(response.data);
-      //   return response.data;
-      // });
+      // 입력값 초기화
+      this.authCode = "";
+
       // 로그인처리 로직
-      if(this.authCode == 1111){
+      await this.sendCode(this.authCode)
+      if(window.accessYn == true){
+        console.log("로그인성공!");
         login();
       }else{
         this.errMsg = "잘못된 코드";
+        return;
       }
     },
+    sendCode : async function(authCode){
+      result = await axios.get('https://plater.kr/api/memberall').then(function(response){
+        console.log(response.data);
+        return response.data;
+      });
+      if(result != null){
+        window.accessYn = true;
+      }else{
+        window.accessYn = false;
+      }
+    }
   }
 });
