77883f555292bc54ddb4f418395b85b9ca830777
전민수 <79839332+alstntjddms@users.noreply.github.com>
2023-04-07 13:33:35 +0900

    로직 정리

--- WebContent/login/index.js
+++ WebContent/login/index.js
@@ -8,29 +8,24 @@ var b = new Vue({
   },
   methods:{
     login : async function(){
-      // 입력값 초기화
-      this.authCode = "";
-
       // 로그인처리 로직
-      await this.sendCode(this.authCode)
-      if(window.accessYn == true){
+      const result = await this.sendCode(this.authCode);
+      if(result != null){
         console.log("로그인성공!");
+        accessYn = true;
         login();
       }else{
         this.errMsg = "잘못된 코드";
-        return;
+        accessYn = false;
       }
+      // 입력값 초기화
+      this.authCode = "";
     },
     sendCode : async function(authCode){
-      result = await axios.get('https://plater.kr/api/memberall').then(function(response){
+      return await axios.get('https://plater.kr/api/memberall').then(function(response){
         console.log(response.data);
         return response.data;
       });
-      if(result != null){
-        window.accessYn = true;
-      }else{
-        window.accessYn = false;
-      }
     }
   }
 });
