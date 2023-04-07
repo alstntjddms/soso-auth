c47b21199adeec813989e2b30d6925567da2bd7c
전민수 <79839332+alstntjddms@users.noreply.github.com>
2023-04-07 17:21:02 +0900

    1

---
+++ target/soso_web-0.0.1-SNAPSHOT/pages/member/index.js
@@ -0,0 +1,20 @@
+var b = new Vue({
+  el: '#app',
+  data: {
+    members: [
+    ],
+    memberCount: 0,
+  },
+  mounted : async function(){
+      await this.load();
+      this.memberCount = this.members.length;
+  },
+  methods:{
+    load : async function(){
+      this.members = await axios.get('https://plater.kr/api/memberall').then(function(response){
+        console.log(response.data);
+        return response.data;
+      })
+    },
+  }
+});
