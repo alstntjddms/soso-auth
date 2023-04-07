267c59b9831e0e449025b8169c613b024d604b55
전민수 <79839332+alstntjddms@users.noreply.github.com>
2023-04-07 16:23:24 +0900

    파일구조 수정

--- WebContent/index.js
+++ WebContent/index.js
@@ -2,46 +2,46 @@
 var accessYn = false;
 
 // 기본페이지 로딩
-$("#main").load("./login/index.html");
+$("#main").load("./pages/login/index.html");
 
 // 로그인 처리
 function login(){
     if (accessYn == false) return;
     $('body').css('background-image', "none");
     $("#sidebar").load("./sidebar/sidebar.html");
-    $("#main").load("./member/index.html");
+    $("#main").load("./pages/member/index.html");
 };
 
 // 메뉴 클릭시 메인페이지 이벤트
 function loadMember(){
     if (accessYn == false) return;
-    $("#main").load("./member/index.html");
+    $("#main").load("./pages/member/index.html");
 };
 function test2(){
     if (accessYn == false) return;
-    $("#main").load("./test2/index.html");
+    $("#main").load("./pages/kakao/index.html");
 };
 function test3(){
     if (accessYn == false) return;
-    $("#main").load("./test3/index.html");
+    $("#main").load("./pages/letter/index.html");
 };
 function test4(){
     if (accessYn == false) return;
-    $("#main").load("./test4/index.html");
+    $("#main").load("./pages/test4/index.html");
 };
 function test5(){
     if (accessYn == false) return;
-    $("#main").load("./test5/index.html");
+    $("#main").load("./pages/test5/index.html");
 };
 function test6(){
     if (accessYn == false) return;
-    $("#main").load("./test6/index.html");
+    $("#main").load("./pages/test6/index.html");
 };
 
 // 홈 클릭시 초기화
 function home(){
     accessYn = false;
-    $("#main").load("./login/index.html");
+    $("#main").load("./pages/login/index.html");
     $('body').css('background-image', "url('background.png')");
     $("#sidebar").html("");
 };
--- target/soso_web-0.0.1-SNAPSHOT/framework/computed.js
+++
@@ -1,4 +0,0 @@
-function timestampToDate(timestamp) {
-  const d = new Date(timestamp);
-  return d.toISOString().slice(0, 19).replace('T', ' ');
-}
