9a8a90f932fb4ba9fa6713faf0713d6be753034c
전민수 <79839332+alstntjddms@users.noreply.github.com>
2023-04-10 23:06:38 +0900

    pwa 로직추가

--- target/soso_web-0.0.1-SNAPSHOT/index.js
+++ target/soso_web-0.0.1-SNAPSHOT/index.js
@@ -13,6 +13,8 @@ function login(){
     $('body').css('background-image', "none");
     $("#sidebar").load("./sidebar/sidebar.html");
     $("#main").load("./pages/member/index.html");
+
+    $("#install-button").hide();
 };
 
 // 메뉴 클릭시 메인페이지 이벤트
@@ -48,4 +50,93 @@ function home(){
     $("#main").load("./pages/login/index.html");
     $('body').css('background-image', "url('background.png')");
     $("#sidebar").html("");
+    $("#install-button").show();
 };
+
+// let deferredInstallPrompt = null;
+// $(document).ready(function(){
+//     console.log("aaa");
+//     $("#install-button").on("click", installPWA);
+// });
+
+// $(window).on("beforeinstallprompt", function(evt){
+//     deferredInstallPrompt = evt.originalEvent;
+//     console.log("aaa1");
+//     $("#install-button").show();
+// });
+
+// function installPWA(){
+//     console.log("aaa2");
+//     deferredInstallPrompt.prompt();
+//     $("#install-button").remove();
+
+//     deferredInstallPrompt.userChoice
+//     .then((choice) =>{
+//         if(choice.outcome === 'accepted'){
+//             console.log("aaa");
+//         }else{
+//             console.log("bbb");
+//         }
+//         deferredInstallPrompt = null;
+//     });
+// }
+
+let deferredPrompt;
+
+self.addEventListener("install", function () {
+  console.log("install");
+});
+
+self.addEventListener("activate", function () {
+  console.log("activate");
+});
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can install the PWA
+  $("#install-button").show();
+  console.log(`'beforeinstallprompt' event was fired.`);
+});
+
+$("#install-button").on('click', async () => {
+  // Hide the app provided install promotion
+  // Show the install prompt
+  console.log("11111");
+  deferredPrompt.prompt();
+  console.log("22222");
+  try {
+    // Wait for the user to respond to the prompt
+    const { outcome } = await deferredPrompt.userChoice;
+    // Optionally, send analytics event with outcome of user choice
+    console.log(`User response to the install prompt: ${outcome}`);
+  } catch (error) {
+    console.log(`Error occurred while handling install prompt: ${error}`);
+  } finally {
+    // We've used the prompt, and can't use it again, throw it away
+    deferredPrompt = null;
+  }
+});
+
+window.addEventListener('appinstalled', () => {
+  // Clear the deferredPrompt so it can be garbage collected
+  deferredPrompt = null;
+  // Optionally, send analytics event to indicate successful install
+  console.log('PWA was installed');
+  $("#install-button").hide();
+});
+
+
+  function getPWADisplayMode() {
+    const isStandalone = window.matchMedia('(display-mode: standalone)').matches;
+    if (document.referrer.startsWith('android-app://')) {
+      return 'twa';
+    } else if (navigator.standalone || isStandalone) {
+      return 'standalone';
+    }
+    return 'browser';
+  }
+
+  console.log(getPWADisplayMode());
